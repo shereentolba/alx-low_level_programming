585ce63123637530ddde4dd61b613ccd38cde9dc
shereentolba <shereentolba2022@gmail.com>
2023-10-05 16:11:34 -0700

    bb

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(void){
-	puts("\" Programming is like building a multilingual puzzle")
-return (0);
-}
 
+/**
+ *main - Entry point
+ *
+ *description : A C program that prints with put function
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
